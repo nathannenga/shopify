049eb187a8e43fb8479668b4fa7f6769f791a0ed
Daniel Falabella <dnlfala@gmail.com>
2016-12-12 21:26:49 -0700

    Added skeleton for get started page

--- controllers/Pages.js
+++ controllers/Pages.js
@@ -2,11 +2,15 @@ var Routes      = module.exports = {};
     // UserCtrl    = require('./UserController');
 
 Routes.index = function (req, res) {
-  if (req.user && req.user._id) return res.redirect('/dashboard');
+  if (req.user && req.user._id) return res.redirect('/get-started');
   return res.render('index'); //res.redirect('/home');
 };
 
 Routes.home = function (req, res) {
   // TODO: Consider deprecating this route
   return res.render('index');
+};
+
+Routes.getStarted = function (req, res) {
+  return res.render('get-started');
 };
--- controllers/Routes.js
+++ controllers/Routes.js
@@ -3,10 +3,12 @@ var Pages    = require('./Pages.js'),
 
 module.exports = function (app, passport) {
   app.get('/', Pages.index);
+  app.get('/get-started', Pages.getStarted);
+  // app.get('/dashboard', Pages.dashboard);
 
   // process the signup form
   app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect : '/dashboard', // redirect to the secure profile section
+    successRedirect : '/get-started', // redirect to the secure profile section
     failureRedirect : '/fail', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
