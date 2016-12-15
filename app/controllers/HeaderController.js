80d5fff2b4d49c1ab4d2c891a51696731306398e
Daniel Falabella <dnlfala@gmail.com>
2016-12-15 06:41:36 -0700

    Added header in admin page

--- app/controllers/HeaderController.js
+++ app/controllers/HeaderController.js
@@ -1,5 +1,27 @@
 angular.module('Shopify')
 
-.controller('HeaderController', ['$scope', 'apiService', function ($scope, apiService) {
+.controller('HeaderController', ['$scope', '$state',
+function ($scope, $state) {
+  $scope.currState = $state;
+  $scope.$watch('currState.current.name', function(newValue, oldValue) {
+    switch (newValue) {
+      case 'products':
+        productsON()
+        break;
+      case 'orders':
+        ordersON()
+        break;
+    }
+  });
+
+  function productsON () {
+    $scope.headerCa = 'Add product';
+    $scope.activeTab = 'products';
+  };
+
+  function ordersON () {
+    $scope.headerCa = 'Create order';
+    $scope.activeTab = 'orders';
+  };
 
 }]);
